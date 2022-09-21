cf9a03a12b79df0805860642d3c115a534143eef
ardilalukita14 <lukitasari623@gmail.com>
2022-09-21 22:58:37 +0700

    section dashboard admin

--- resources/views/admin/appnew.blade.php
+++ resources/views/admin/appnew.blade.php
@@ -17,26 +17,60 @@
       <meta name="keywords" content="bootstrap, bootstrap admin template, admin theme, admin dashboard, dashboard template, admin template, responsive" />
       <meta name="author" content="codedthemes" />
       <!-- Favicon icon -->
-      <link rel="icon" href="{{ asset('backend/assets/images/favicon.ico')}}" type="image/x-icon">
+      <link rel="icon" href="backend/assets/images/favicon.ico" type="image/x-icon">
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
     <!-- waves.css -->
-    <link rel="stylesheet" href="{{ asset('backend/assets/pages/waves/css/waves.min.css')}}" type="text/css" media="all">
+    <link rel="stylesheet" href="backend/assets/pages/waves/css/waves.min.css" type="text/css" media="all">
       <!-- Required Fremwork -->
-      <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/css/bootstrap/css/bootstrap.min.css')}}">
+      <link rel="stylesheet" type="text/css" href="backend/assets/css/bootstrap/css/bootstrap.min.css">
       <!-- waves.css -->
-      <link rel="stylesheet" href="{{ asset('backend/assets/pages/waves/css/waves.min.css')}}" type="text/css" media="all">
+      <link rel="stylesheet" href="backend/assets/pages/waves/css/waves.min.css" type="text/css" media="all">
       <!-- themify icon -->
-      <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/icon/themify-icons/themify-icons.css')}}">
+      <link rel="stylesheet" type="text/css" href="backend/assets/icon/themify-icons/themify-icons.css">
       <!-- Font Awesome -->
-      <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/icon/font-awesome/css/font-awesome.min.css')}}">
+      <link rel="stylesheet" type="text/css" href="backend/assets/icon/font-awesome/css/font-awesome.min.css">
       <!-- scrollbar.css -->
-      <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/css/jquery.mCustomScrollbar.css')}}">
+      <link rel="stylesheet" type="text/css" href="backend/assets/css/jquery.mCustomScrollbar.css">
         <!-- am chart export.css -->
         <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
       <!-- Style.css -->
-      <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/css/style.css')}}">
+      <link rel="stylesheet" type="text/css" href="backend/assets/css/style.css">
   </head>
+
+      <!-- Required Jquery -->
+      <script type="text/javascript" src="backend/assets/js/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="backend/assets/js/jquery-ui/jquery-ui.min.js "></script>
+    <script type="text/javascript" src="backend/assets/js/popper.js/popper.min.js"></script>
+    <script type="text/javascript" src="backend/assets/js/bootstrap/js/bootstrap.min.js "></script>
+    <script type="text/javascript" src="backend/assets/pages/widget/excanvas.js "></script>
+    <!-- waves js -->
+    <script src="backend/assets/pages/waves/js/waves.min.js"></script>
+    <!-- jquery slimscroll js -->
+    <script type="text/javascript" src="backend/assets/js/jquery-slimscroll/jquery.slimscroll.js "></script>
+    <!-- modernizr js -->
+    <script type="text/javascript" src="backend/assets/js/modernizr/modernizr.js "></script>
+    <!-- slimscroll js -->
+    <script type="text/javascript" src="backend/assets/js/SmoothScroll.js"></script>
+    <script src="backend/assets/js/jquery.mCustomScrollbar.concat.min.js "></script>
+    <!-- Chart js -->
+    <script type="text/javascript" src="backend/assets/js/chart.js/Chart.js"></script>
+    <!-- amchart js -->
+    <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+    <script src="backend/assets/pages/widget/amchart/gauge.js"></script>
+    <script src="backend/assets/pages/widget/amchart/serial.js"></script>
+    <script src="backend/assets/pages/widget/amchart/light.js"></script>
+    <script src="backend/assets/pages/widget/amchart/pie.min.js"></script>
+    <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
+    <!-- menu js -->
+    <script src="backend/assets/js/pcoded.min.js"></script>
+    <script src="backend/assets/js/vertical-layout.min.js "></script>
+    <!-- custom js -->
+    <script type="text/javascript" src="backend/assets/pages/dashboard/custom-dashboard.js"></script>
+    <script type="text/javascript" src="backend/assets/js/script.js "></script>
+</body>
+
+</html>
 
   <body>
   <!-- Required Jquery -->
