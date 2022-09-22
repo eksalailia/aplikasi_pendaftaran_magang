36a5efb6cf5bf8806f124149c6aa2d8017d3fd5a
ardilalukita14 <lukitasari623@gmail.com>
2022-09-22 17:41:38 +0700

    validation for login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -55,8 +55,8 @@ class LoginController extends Controller
     public function login(Request $request){
        $input = $request->all();
        $this->validate($request,[
-           'email'=>'required|email',
-           'password'=>'required'
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
        ]);
 
        if( auth()->attempt(array('email'=>$input['email'], 'password'=>$input['password'])) ){
