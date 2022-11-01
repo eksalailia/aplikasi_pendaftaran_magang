85e90f222e511f36b6e182869c1a4aa3f9232818
eksalailia <eksalailia@gmail.com>
2022-11-01 07:37:02 +0700

    dashboard mentor

--- resources/views/mentor/main.blade.php
+++ resources/views/mentor/main.blade.php
@@ -55,7 +55,7 @@
 						<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
 							<div>
 								<h2 class="text-white pb-2 fw-bold">Dashboard</h2>
-								<h5 class="text-white op-7 mb-2">Informasi Umum Terkait Halaman mentor</h5>
+								<h5 class="text-white op-7 mb-2">Informasi Umum Terkait Halaman Mentor</h5>
 							</div>
 						</div>
 					</div>
